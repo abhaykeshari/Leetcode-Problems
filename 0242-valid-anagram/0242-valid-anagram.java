21f5add76cc04dd59ada5919b848ecea21218348
Abhay kesharwani <shiyalal11@gmail.com>
2026-07-27 20:04:33 +0530

    Time: 3 ms (93.61%), Space: 46.7 MB (16.49%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char arr1[] = s.toCharArray();
+        char arr2[] = t.toCharArray();
+        if(s.length() != t.length()){
+            return false;
+        }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1,arr2);
+    }
+}
